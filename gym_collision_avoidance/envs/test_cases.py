38a7a3febdf350f1ac27481388826b74a44aa20a
bdebrito <rbuno.debrito@tudelft.nl>
2020-07-02 17:06:04 +0200

    adding right dynamics depending on policy

--- gym_collision_avoidance/envs/test_cases.py
+++ gym_collision_avoidance/envs/test_cases.py
@@ -485,7 +485,7 @@ def train_agents_swap_circle(test_case_index, number_of_agents=5, agents_policy=
         cooperation_coef = np.random.uniform(0.0, 1.0)
         if ag_id == 0:
             agents.append(Agent(positions_list[ag_id][0], positions_list[ag_id][1],
-                                positions_list[ag_id+1][0], positions_list[ag_id+1][1], radius, pref_speed, None, agents_policy, UnicycleDynamics,
+                                positions_list[ag_id+1][0], positions_list[ag_id+1][1], radius, pref_speed, None, agents_policy, agents_dynamics,
                       [OtherAgentsStatesSensor], 0))
         else:
             agents.append(Agent(positions_list[2*ag_id][0], positions_list[2*ag_id][1],
