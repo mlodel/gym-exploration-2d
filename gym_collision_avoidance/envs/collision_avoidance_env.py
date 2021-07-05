fixes to make training run, !!! reward normalization commented out!

gym_collision_avoidance/envs/collision_avoidance_env.py
@@ -84,7 +84,7 @@ class CollisionAvoidanceEnv(gym.Env):
         self.number_of_agents = 2
         self.scenario = Config.SCENARIOS_FOR_TRAINING
 
-        self.ego_policy = "RVOPolicy"
+        self.ego_policy = "MPCRLStaticObsIGPolicy"
 
         # self.ego_policy = "MPCRLStaticObsPolicy"
         self.ego_agent_dynamics = "UnicycleSecondOrderEulerDynamics"
@@ -556,13 +556,15 @@ class CollisionAvoidanceEnv(gym.Env):
                 # if gets close to goal
                 rewards[i] += Config.REWARD_DISTANCE_TO_GOAL * (agent.past_dist_to_goal - agent.dist_to_goal)
 
-                if hasattr(agent.policy, "targetMap"):
-                    ig_reward = agent.policy.targetMap.get_reward_from_pose(np.append(agent.pos_global_frame,
-                                                                                      agent.heading_global_frame))
+                if agent.ig_model is not None:
+                    ig_reward = agent.ig_model.get_reward(agent.pos_global_frame, agent.heading_global_frame)
+                    # ig_reward = agent.policy.targetMap.get_reward_from_pose(np.append(agent.pos_global_frame,
+                    #                                                                   agent.heading_global_frame))
                     rewards[i] += ig_reward
 
-        rewards = np.clip(rewards, self.min_possible_reward,
-                          self.max_possible_reward) / (self.max_possible_reward - self.min_possible_reward)
+        # rewards = np.clip(rewards, self.min_possible_reward,
+        #                   self.max_possible_reward) / (self.max_possible_reward - self.min_possible_reward)
+
         if Config.TRAIN_SINGLE_AGENT:
             rewards = rewards[0]
         return rewards

gym_collision_avoidance/envs/config.py
@@ -59,7 +59,7 @@ class Config:
     NUM_PAST_ACTIONS_IN_STATE = 0
 
     NEAR_GOAL_THRESHOLD = 0.75
-    MAX_TIME_RATIO = 100.0  # agent has this number times the straight-line-time to reach its goal before "timing out"
+    MAX_TIME_RATIO = 5.0  # agent has this number times the straight-line-time to reach its goal before "timing out"
 
     SENSING_HORIZON = np.inf
     # SENSING_HORIZON  = 3.0
@@ -88,8 +88,8 @@ class Config:
     # STATIC MAP
     MAP_WIDTH = 30  # Meters
     MAP_HEIGHT = 30  # Meters
-    MAP_WIDTH_PXL = int(MAP_WIDTH/SUBMAP_RESOLUTION)
-    MAP_HEIGHT_PXL = int(MAP_HEIGHT/SUBMAP_RESOLUTION)
+    MAP_WIDTH_PXL = 60
+    MAP_HEIGHT_PXL = 60
 
 
     SCENARIOS_FOR_TRAINING = [

gym_collision_avoidance/envs/test_cases.py
@@ -36,6 +36,8 @@ from gym_collision_avoidance.envs.policies.LearningPolicy import LearningPolicy
 from gym_collision_avoidance.envs.policies.CARRLPolicy import CARRLPolicy
 from mpc_rl_collision_avoidance.policies.MPCPolicy import MPCPolicy
 from mpc_rl_collision_avoidance.policies.MPC_IG_Policy import MPC_IG_Policy
+from mpc_rl_collision_avoidance.policies.MPCRLStaticObsIGPolicy import MPCRLStaticObsIGPolicy
+
 from mpc_rl_collision_avoidance.policies.MPCRLStaticObsPolicy import MPCRLStaticObsPolicy
 from mpc_rl_collision_avoidance.policies.MPCStaticObsPolicy import MPCStaticObsPolicy
 from mpc_rl_collision_avoidance.policies.SecondOrderMPCPolicy import SecondOrderMPCPolicy
@@ -100,10 +102,10 @@ def IG_single_agent():
 
     return agents, obstacle
 
-def IG_single_agent_crossing(number_of_agents=1, ego_agent_policy=NonCooperativePolicy,
+def IG_single_agent_crossing(number_of_agents=1, ego_agent_policy=MPCRLStaticObsIGPolicy,
                              other_agents_policy=NonCooperativePolicy, ego_agent_dynamics=FirstOrderDynamics,
                              other_agents_dynamics=UnicycleDynamics, agents_sensors=[], seed=None, obstacle=None):
-    pref_speed = 4.0  # np.random.uniform(1.0, 0.5)
+    pref_speed = 5.0  # np.random.uniform(1.0, 0.5)
     radius = 0.5  # np.random.uniform(0.5, 0.5)
     agents = []
     # if seed:
@@ -124,8 +126,8 @@ def IG_single_agent_crossing(number_of_agents=1, ego_agent_policy=NonCooperative
     obstacle.extend([obstacle_1, obstacle_2, obstacle_3, obstacle_4, obstacle_5, obstacle_6, obstacle_7, obstacle_8])
 
     # ego agent
-    agents.append(Agent(0, 0, 15, 15, radius, pref_speed, 0, MPC_IG_Policy, UnicycleSecondOrderEulerDynamics,
-                        [OtherAgentsStatesSensor, LaserScanSensor], 0, ig_model=ig_agent))
+    agents.append(Agent(0, 0, 50, 0, radius, pref_speed, 0, ego_agent_policy, UnicycleSecondOrderEulerDynamics,
+                        [OtherAgentsStatesSensor, OccupancyGridSensor], 0, ig_model=ig_agent))
     # target agent
     agents.append(Agent(10, 0, 0, 0, 0.2, pref_speed, 0, StaticPolicy, FirstOrderDynamics, [], 1))
     agents.append(Agent(-6, -12, 0, 0, 0.2, pref_speed, 0, StaticPolicy, FirstOrderDynamics, [], 2))