visualization updated for mpc and goal policy, new action flag for REPEAT_STEP loop to reuse goal/action in substeps, entropy computation corrected

gym_collision_avoidance/envs/collision_avoidance_env.py
@@ -199,7 +199,7 @@ class CollisionAvoidanceEnv(gym.Env):
         elif actions.size == 0 and not self.use_expert_goal:
             actions = self.agents[0].goal_global_frame - self.agents[0].pos_global_frame
             actions = np.clip(actions, self.action_space.low, self.action_space.high)
-
+        new_action = True
         for _ in range(Config.REPEAT_STEPS):
 
             self.episode_step_number += 1
@@ -210,8 +210,8 @@ class CollisionAvoidanceEnv(gym.Env):
                 self._prediction_step()
 
             # Take action
-            self._take_action(actions, dt)
-
+            self._take_action(actions, dt, new_action)
+            new_action = False
             # Collect rewards
             step_rewards = self._compute_rewards()
             rewards += step_rewards
@@ -335,7 +335,7 @@ class CollisionAvoidanceEnv(gym.Env):
             agent.policy.predicted_trajectory = self.predicted_trajectory[indices != id]
             agent.policy.all_predicted_trajectory = self.predicted_trajectory
 
-    def _take_action(self, actions, dt):
+    def _take_action(self, actions, dt, new_action=True):
         num_actions_per_agent = self.agents[0].dynamics_model.num_actions
         all_actions = np.zeros((len(self.agents), num_actions_per_agent), dtype=np.float32)
 
@@ -347,7 +347,7 @@ class CollisionAvoidanceEnv(gym.Env):
             if agent.policy.is_external:
                 all_actions[agent_index, :] = agent.policy.convert_to_action(actions[agent_index])
             elif agent.policy.is_still_learning:
-                all_actions[agent_index, :] = agent.policy.network_output_to_action(agent_index, self.agents, actions)
+                all_actions[agent_index, :] = agent.policy.network_output_to_action(agent_index, self.agents, actions, new_action)
             elif "ig_mcts" in str(agent.policy):
                 dmcts_agents.append(agent_index)
             else:

gym_collision_avoidance/envs/config.py
@@ -26,7 +26,7 @@ class Config:
     #########################################################################
     # COLLISION AVOIDANCE PARAMETER
     NUM_TEST_CASES = 50
-    PLOT_EVERY_N_EPISODES = 1000  # for tensorboard visualization
+    PLOT_EVERY_N_EPISODES = 500  # for tensorboard visualization
     DT = 0.1  # seconds between simulation time steps
     REWARD_AT_GOAL = 0.0  # reward given when agent reaches goal position
     REWARD_COLLISION_WITH_AGENT = 0.0  # reward given when agent collides with another agent
@@ -223,7 +223,7 @@ class Config:
             'dtype': np.float32,
             'size': (MAP_WIDTH_PXL, MAP_HEIGHT_PXL),
             'bounds': [-np.inf, np.inf],
-            'attr': 'ig_model.targetMap.map',
+            'attr': 'ig_model.targetMap.entropyMap',
             'std': np.ones( (MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.float32 ),
             'mean': np.ones( (MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.float32 ),
         },

gym_collision_avoidance/envs/information_models/ig_agent.py
@@ -89,7 +89,7 @@ class ig_agent():
 
         return targets
 
-    def get_greedy_goal(self, pose, max_dist=4.0, min_dist=0.0, Nsamples=30):
+    def get_greedy_goal(self, pose, max_dist=4.0, min_dist=1.0, Nsamples=30):
         # Generate candidate goals in polar coordinates + yaw angle
         # np.random.seed(10)
         candidates_polar = np.random.rand(Nsamples,2)

gym_collision_avoidance/envs/information_models/targetMap.py
@@ -31,7 +31,7 @@ class targetMap():
 
         self.logMap_bound = logmap_bound
 
-        self.entropyMap = np.ones(shape) * ( -p_prior*np.log(p_prior) - (1-p_prior)*np.log(1-p_prior) )
+        self.entropyMap = np.ones(shape) * ( -p_prior*np.log(p_prior) - (1-p_prior)*np.log(1-p_prior) ) / np.log(2)
 
     def getCellsFromPose(self, pose):
         if len(pose) > 2:
@@ -152,9 +152,9 @@ class targetMap():
                 self.probMap[j,i] = p_cell
 
                 # Update Entropies and obtain reward
-                # cell_entropy = -p_cell*np.log(p_cell) - (1-p_cell)*np.log(1-p_cell)
+                cell_entropy = (-p_cell*np.log(p_cell) - (1-p_cell)*np.log(1-p_cell)) / np.log(2)
                 # reward += self.entropyMap[j,i] - cell_entropy
-                # self.entropyMap[j,i] = cell_entropy
+                self.entropyMap[j,i] = cell_entropy
             obsvdCells.update(visibleCells)
         return obsvdCells, reward
 

gym_collision_avoidance/envs/visualize.py
@@ -265,7 +265,7 @@ def draw_agents(agents, obstacle, circles_along_traj, ax, ax2=None, last_index=-
 
     for i, agent in reversed(list(enumerate(agents))):
 
-        if "ig_" in str(type(agent.policy)):
+        if agent.ig_model is not None:
             draw_agent_ig(agent, i, ax)
             continue
 
@@ -476,22 +476,39 @@ def draw_agent_ig(agent, i, ax):
             agent.global_state_history[:agent.step_num - 1, 2],
             color=plt_color)
 
-    plan = agent.policy.best_paths.X[0].pose_seq
-    fov = 60.0
-    for j in range(len(plan)):
-        if j == 0:
-            continue
-        pose = plan[j]
-        alpha = 1.0 - 0.2 * j
+    fov = agent.ig_model.detect_fov * 180.0 / np.pi
+
+    if hasattr(agent.policy, "best_paths"):
+        plan = agent.policy.best_paths.X[0].pose_seq
+
+        for j in range(len(plan)):
+            if j == 0:
+                continue
+            pose = plan[j]
+            alpha = 1.0 - 0.2 * j
+            c = rgba2rgb(plt_color + [float(alpha)])
+            heading = pose[2] * 180.0 / np.pi
+            ax.add_patch(Wedge(center=pose[0:2], r=.75, theta1=(heading - fov / 2), theta2=(heading + fov / 2), fc=c, ec=c,
+                               fill=True))
+
+    if hasattr(agent.policy, "goal_"):
+        pose = agent.policy.goal_
+        alpha = 0.5
         c = rgba2rgb(plt_color + [float(alpha)])
-        heading = pose[2] * 180.0 / np.pi
-        ax.add_patch(Wedge(center=pose[0:2], r=.75, theta1=(heading - fov / 2), theta2=(heading + fov / 2), fc=c, ec=c,
+        heading = 0.0
+        ax.add_patch(Wedge(center=pose[0:2], r=.4, theta1=(heading - fov / 2), theta2=(heading + fov / 2), fc=c, ec=c,
                            fill=True))
+
+    if hasattr(agent.policy, "predicted_traj"):
+        ax.plot(agent.policy.predicted_traj[:,0], agent.policy.predicted_traj[:,1], color=plt_color, alpha=0.5)
+
     # currentPose = agent.global_state_history[agent.step_num-1, (1,2,10)]
-    currentPose = plan[0]
-    heading = currentPose[2] * 180.0 / np.pi
-    ax.add_patch(Wedge(center=currentPose[0:2], r=1., theta1=(heading - fov / 2),
-                       theta2=(heading + fov / 2), fc=plt_color, ec=plt_color, fill=True))
+    currentPose = agent.pos_global_frame
+    heading = agent.heading_global_frame * 180.0 / np.pi
+    alpha = 0.6
+    c = rgba2rgb(plt_color + [float(alpha)])
+    ax.add_patch(Wedge(center=currentPose[0:2], r=0.5, theta1=(heading - fov / 2),
+                       theta2=(heading + fov / 2), fc=c, ec=plt_color, fill=True))
 
 
 def plot_Angular_map_vector(ax2, Angular_Map, ag, max_range=6):