Reward Normalization, agent_pos_map, fixes for mcts+mpc, fixes for target detection

gym_collision_avoidance/envs/agent.py
@@ -261,7 +261,7 @@ class Agent(object):
     def get_observation_dict(self, agents):
         observation = {}
         for state in Config.STATES_IN_OBS:
-            if self.ig_model is None and state == 'target_map':
+            if self.ig_model is None and (state == 'target_map' or state == 'agent_pos_map'):
                 continue
             observation[state] = np.array(eval("self." + Config.STATE_INFO_DICT[state]['attr']))
         return observation

gym_collision_avoidance/envs/collision_avoidance_env.py
@@ -306,7 +306,7 @@ class CollisionAvoidanceEnv(gym.Env):
                 agent.ig_model.init_model(occ_map=self.map,
                                           map_size=(Config.MAP_WIDTH, Config.MAP_HEIGHT),
                                           map_res=Config.SUBMAP_RESOLUTION,
-                                          detect_fov=360.0, detect_range=3.0)
+                                          detect_fov=360.0, detect_range=2.5)
 
         for state in Config.STATES_IN_OBS:
             for agent in range(Config.MAX_NUM_AGENTS_IN_ENVIRONMENT):
@@ -605,6 +605,10 @@ class CollisionAvoidanceEnv(gym.Env):
                     #                                                                   agent.heading_global_frame))
                     rewards[i] += ig_reward
 
+                # if agent.step_num > 5:
+                #     distance = np.linalg.norm(agent.pos_global_frame - agent.global_state_history[:agent.step_num - 5, 1:2])
+                #     rewards[i] += - distance*0.001
+
                 # If subgoal position in inside an obstacle
                 """ """
                 if i == 0:
@@ -618,8 +622,8 @@ class CollisionAvoidanceEnv(gym.Env):
                         rewards[i] += -0.1
 
                 # rewards[i] += 0.01 * agent.speed_global_frame
-        # rewards = np.clip(rewards, self.min_possible_reward,
-        #                   self.max_possible_reward) / (self.max_possible_reward - self.min_possible_reward)
+        rewards = np.clip(rewards, self.min_possible_reward,
+                          self.max_possible_reward) / (self.max_possible_reward - self.min_possible_reward)
 
         if Config.TRAIN_SINGLE_AGENT:
             rewards = rewards[0]
@@ -824,24 +828,38 @@ class CollisionAvoidanceEnv(gym.Env):
         self.reward_getting_close = Config.REWARD_GETTING_CLOSE
         self.reward_entered_norm_zone = Config.REWARD_ENTERED_NORM_ZONE
         self.reward_time_step = Config.REWARD_TIME_STEP
+        self.reward_timeout = Config.REWARD_TIMEOUT
 
         self.reward_wiggly_behavior = Config.REWARD_WIGGLY_BEHAVIOR
         self.wiggly_behavior_threshold = Config.WIGGLY_BEHAVIOR_THRESHOLD
+        self.reward_max_ig = Config.REWARD_MAX_IG
 
-        self.possible_reward_values = \
+        self.possible_terminal_reward_values = \
             np.array([self.reward_at_goal,
                       self.reward_collision_with_agent,
-                      self.reward_time_step,
                       self.reward_collision_with_wall,
-                      self.reward_wiggly_behavior
+                      self.reward_timeout
                       ])
-        self.min_possible_reward = np.min(self.possible_reward_values)
-        self.max_possible_reward = np.max(self.possible_reward_values)
+
+        self.possible_step_reward_values = [self.reward_max_ig]
+
+        self.min_possible_reward = Config.REPEAT_STEPS * self.reward_time_step \
+                                                          + sum(
+                    [r if r < 0 else 0 for r in self.possible_step_reward_values]) \
+                                   + np.min(self.possible_terminal_reward_values)
+
+        self.max_possible_reward = Config.REPEAT_STEPS * self.reward_time_step \
+                                                          + sum(
+                    [r if r > 0 else 0 for r in self.possible_step_reward_values]) \
+                                   + np.max(self.possible_terminal_reward_values)
+
+
+
 
     def get_expert_goal(self):
-        goal = self.agents[0].ig_model.expert_policy.get_expert_goal()[0:2]\
-               - self.agents[0].pos_global_frame
-        # goal, exitflag = self.agents[0].policy.mpc_output(0, self.agents)
+        # goal = self.agents[0].ig_model.expert_policy.get_expert_goal()[0:2]\
+        #        - self.agents[0].pos_global_frame
+        goal, exitflag = self.agents[0].policy.mpc_output(0, self.agents)
         return goal
 
     def set_plot_save_dir(self, plot_save_dir):

gym_collision_avoidance/envs/config.py
@@ -85,6 +85,8 @@ class Config:
 
     IG_EXPERT_POLICY = "IG_EXPERT_POLICY"
 
+    REWARD_MAX_IG = 6.0
+
 
     SCENARIOS_FOR_TRAINING = [
         "IG_single_agent_crossing"]  # ["train_agents_swap_circle","train_agents_random_positions","train_agents_pairwise_swap"]
@@ -94,7 +96,7 @@ class Config:
     MAX_RANGE = 6
 
     PLOT_CIRCLES_ALONG_TRAJ = False
-    ANIMATION_PERIOD_STEPS = 5  # plot every n-th DT step (if animate mode on)
+    ANIMATION_PERIOD_STEPS = 1  # plot every n-th DT step (if animate mode on)
     PLT_LIMITS = ((-MAP_WIDTH/2, MAP_WIDTH/2), (-MAP_HEIGHT/2, MAP_HEIGHT/2))
     PLT_FIG_SIZE = (12, 8)
     PLT_SHOW_LEGEND = False
@@ -103,7 +105,8 @@ class Config:
     PLT_FREE_SPACE = True
 
     # STATES_IN_OBS = ['dist_to_goal', 'rel_goal', 'radius', 'heading_ego_frame', 'pref_speed', 'other_agents_states']
-    STATES_IN_OBS = ['radius', 'heading_global_frame', 'pos_global_frame', 'local_grid', 'target_map']  # occupancy grid
+    # STATES_IN_OBS = ['radius', 'heading_global_frame', 'pos_global_frame', 'local_grid', 'target_map']  # occupancy grid
+    STATES_IN_OBS = ['radius', 'heading_global_frame', 'pos_global_frame', 'local_grid', 'agent_pos_map', 'target_map']  # occupancy grid
     # STATES_IN_OBS = ['dist_to_goal', 'rel_goal', 'radius', 'heading_ego_frame', 'pref_speed', 'other_agents_states', 'angular_map'] #angular map
     # STATES_IN_OBS = ['dist_to_goal', 'radius', 'heading_ego_frame', 'pref_speed', 'other_agent_states', 'use_ppo', 'laserscan']
     # STATES_IN_OBS = ['dist_to_goal', 'radius', 'heading_ego_frame', 'pref_speed', 'other_agent_states', 'use_ppo'] # 2-agent net
@@ -230,6 +233,14 @@ class Config:
             'std': np.ones( (MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.float32 ),
             'mean': np.ones( (MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.float32 ),
         },
+        'agent_pos_map': {
+            'dtype': np.float32,
+            'size': (MAP_WIDTH_PXL, MAP_HEIGHT_PXL),
+            'bounds': [-np.inf, np.inf],
+            'attr': 'ig_model.agent_pos_map',
+            'std': np.ones((MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.float32),
+            'mean': np.ones((MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.float32),
+        }
     }
     MEAN_OBS = {}
     STD_OBS = {}

gym_collision_avoidance/envs/information_models/ig_agent.py
@@ -33,6 +33,9 @@ class ig_agent():
 
         self.greedy_goal = None
 
+        self.agent_pos_map = None
+        self.agent_pos_idc = None
+
         # np.random.seed(current_milli_time() - int(1.625e12))
 
     def init_model(self, occ_map, map_size, map_res, detect_fov, detect_range):
@@ -43,8 +46,12 @@ class ig_agent():
         # Init EDF and Target Map
         edf_map_obj = edfMap(occ_map, map_res, map_size)
         self.targetMap = targetMap(edf_map_obj, map_size, map_res * 10,
-                                   sensFOV=self.detect_fov, sensRange=self.detect_range, rOcc=1.1,
-                                   rEmp=0.9)
+                                   sensFOV=self.detect_fov, sensRange=self.detect_range, rOcc=3,
+                                   rEmp=0.333)
+
+        self.agent_pos_map = np.zeros(self.targetMap.map.shape)
+        self.agent_pos_idc = self.targetMap.getCellsFromPose(self.host_agent.pos_global_frame)
+        self.agent_pos_map[self.agent_pos_idc[1], self.agent_pos_idc[0]] = 1.0
 
     def update(self, agents):
 
@@ -57,6 +64,10 @@ class ig_agent():
 
         ig_agents = [i for i in range(len(agents)) if "ig_" in str(type(agents[i].policy)) and i != self.host_agent.id]
         self.update_belief(ig_agents, global_pose, agents, other_agents_states)
+
+        self.agent_pos_map[self.agent_pos_idc[1], self.agent_pos_idc[0]] = 0.0
+        self.agent_pos_idc = self.targetMap.getCellsFromPose(self.host_agent.pos_global_frame)
+        self.agent_pos_map[self.agent_pos_idc[1], self.agent_pos_idc[0]] = 1.0
 
     def get_reward(self, agent_pos, agent_heading):
         return self.targetMap.get_reward_from_pose(np.append(agent_pos, agent_heading))
@@ -82,18 +93,24 @@ class ig_agent():
     def find_targets_in_obs(self, other_agents_states, global_pose):
 
         # Find Targets in Range and FOV (Detector Emulation)
+        c, s = np.cos(global_pose[2]), np.sin(global_pose[2])
+        R = np.array(((c, s), (-s, c)))
         targets = []
         for other_agent in other_agents_states:
             if other_agent[9] == 1.0:
                 # Static Agent = Target
                 # r = other_agent[0:2] - global_pose[0:2]
                 r = other_agent[0:2]
-                dphi = np.arctan2(r[1], r[0]) - global_pose[2]
-                in_fov = abs(dphi) <= self.detect_fov / 2.0
                 r_norm = np.sqrt(r[0] ** 2 + r[1] ** 2)
                 in_range = r_norm <= self.detect_range
-                if in_fov and in_range:
-                    targets.append(other_agent[0:2] + global_pose[0:2])
+                if in_range:
+                    r_rot = np.dot(R, r)
+                    dphi = (np.arctan2(r_rot[1], r_rot[0]))
+                    in_fov = abs(dphi) <= self.detect_fov / 2.0
+                    if in_fov:
+                        targets.append(other_agent[0:2] + global_pose[0:2])
+                    else:
+                        what=1
 
         return targets
 

gym_collision_avoidance/envs/information_models/ig_mcts.py
@@ -42,7 +42,7 @@ class ig_mcts():
         self.mcts_gamma = 0.99
         self.cooperation_coef = 0
         self.DT = 0.1
-        self.xDT = 6
+        self.xDT = 12
 
     def set_param(self, ego_agent, dt=0.1, xdt=1,
                   Ntree=100, Nsims=10, parallelize_sims=False, mcts_cp=1., mcts_horizon=10, mcts_gamma=0.99, Ncycles=5,
@@ -196,8 +196,8 @@ class ig_mcts():
 
     @staticmethod
     def mcts_avail_actions(data, state, robot_id):
-        vel_list = [0.0, 1.0, 3.0]
-        dphi_list = [-0.5*np.pi, 0, 0.5*np.pi]
+        vel_list = [1.0, 3.0]
+        dphi_list = [-0.25*np.pi, -0.1*np.pi, 0, 0.1*np.pi, 0.25*np.pi]
 
         action_list = [np.array([vel, dphi]) for vel in vel_list for dphi in dphi_list]
         return action_list

gym_collision_avoidance/envs/policies/pydecmcts/DecMCTS.py
@@ -37,10 +37,10 @@ class ActionDistribution:
         self.X = X
 
         # Normalise 
-        if sum(q) == 0:
-            self.q = [1 / float(len(q))] * len(q)
-        else:
-            self.q = (np.array(q).astype(float) / sum(q)).tolist()
+        # if sum(q) == 0:
+        #     self.q = [1 / float(len(q))] * len(q)
+        # else:
+        #     self.q = (np.array(q).astype(float) / sum(q)).tolist()
 
     def best_action(self):
         """

gym_collision_avoidance/envs/visualize.py
@@ -134,6 +134,7 @@ def plot_episode(agents, obstacles, in_evaluate_mode,
         ax3 = fig.add_axes([0.72, 0.2, 0.3, 0.3])
         # prob_map = targetMap.map / (targetMap.map + 1)
         ax3.imshow(targetMap.probMap, vmin=0, vmax=1, cmap='jet', origin='lower')
+        # ax3.imshow(agents[0].ig_model.agent_pos_map, cmap=plt.cm.binary, origin='lower')
         ax3.set_yticklabels([])
         ax3.set_xticklabels([])
 
@@ -474,14 +475,14 @@ def draw_agent_ig(agent, i, ax):
     if hasattr(agent.policy, 'static_obstacles_manager') and Config.PLT_FREE_SPACE:
         workspace_constr_a = np.array([[1, 0], [0, 1], [-1, 0], [0, -1]])
         workspace_constr_b = np.array([15, 15, 15, 15])
-        for constr in agent.policy.linear_constraints[0:3]:
-            workspace_constr_a = np.concatenate((workspace_constr_a, np.expand_dims(constr[0], axis=0)))
-            workspace_constr_b = np.concatenate((workspace_constr_b, np.array([constr[1]])))
-            # workspace_constr_a = np.concatenate((workspace_constr_a, np.expand_dims(constr[0:2], axis=0)))
-            # workspace_constr_b = np.concatenate((workspace_constr_b, np.array([constr[2]])))
+        for constr in agent.policy.linear_constraints[0:4]:
+            # workspace_constr_a = np.concatenate((workspace_constr_a, np.expand_dims(constr[0], axis=0)))
+            # workspace_constr_b = np.concatenate((workspace_constr_b, np.array([constr[1]])))
+            workspace_constr_a = np.concatenate((workspace_constr_a, np.expand_dims(constr[0:2], axis=0)))
+            workspace_constr_b = np.concatenate((workspace_constr_b, np.array([constr[2]])))
         try:
             vertices = pypoman.polygon.compute_polygon_hull(workspace_constr_a, workspace_constr_b)
-            ax.add_patch(plt.Polygon(vertices, ec=plt_colors[9], fill=True, alpha=0.5))
+            ax.add_patch(plt.Polygon(vertices, ec=plt_colors[8], fc=plt_colors[8], fill=True, alpha=0.5))
         except:
             print("Something went wrong drawing the polygon")
 