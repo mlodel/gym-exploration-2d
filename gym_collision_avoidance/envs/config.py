fixing reward scaling and observation shapes

gym_collision_avoidance/envs/config.py
@@ -8,7 +8,7 @@ class Config:
     continuous, discrete = range(2)  # Initialize game types as enum
     ACTION_SPACE_TYPE = discrete
 
-    ANIMATE_EPISODES = True
+    ANIMATE_EPISODES = False
     SHOW_EPISODE_PLOTS = False
     SAVE_EPISODE_PLOTS = True
     TRAIN_MODE = False  # Enable to see the trained agent in action (for testing)
@@ -33,7 +33,7 @@ class Config:
     )
     REWARD_TIMEOUT = 0.0  # reward given for not reaching the goal
     REWARD_INFEASIBLE = 0.0
-    REWARD_COLLISION_WITH_WALL = -0.25  # reward given when agent collides with wall
+    REWARD_COLLISION_WITH_WALL = -0.0  # reward given when agent collides with wall
     REWARD_GETTING_CLOSE = (
         0.0  # reward when agent gets close to another agent (unused?)
     )
@@ -41,7 +41,7 @@ class Config:
         0.0  # reward when agent enters another agent's social zone
     )
     REWARD_TIME_STEP = (
-        -0.1
+        -0.0
     )  # -0.1  # default reward given if none of the others apply (encourage speed)
     REWARD_DISTANCE_TO_GOAL = (
         0.0  # default reward given if none of the others apply (encourage speed)
@@ -111,7 +111,6 @@ class Config:
     IG_SENSE_FOV = 360.0
     IG_SENSE_rOcc = 3.0
     IG_SENSE_rEmp = 0.33
-    REWARD_MAX_IG = 1.2  # 6.7 4.0 # 0.2 ## binary 1.2 entropy 4.0 (w/o accumulating)
     IG_ACCUMULATE_REWARDS = False
     IG_REWARD_MODE = "binary"  # entropy, binary
     IG_REWARD_BINARY_CELL = 0.1
@@ -119,9 +118,18 @@ class Config:
     IG_THRES_AVG_CELL_ENTROPY = 0.1  # 0.1
     IG_THRES_ACTIVE = False  # When False fixed episode length by timeout
 
-    IG_REWARD_GOAL_CELL_FACTOR = 10.0
+    IG_REWARD_GOAL_CELL_FACTOR = 3.0
+    IG_REWARD_GOAL_COMPLETION = 0.0
 
     IG_GOALS_SETTINGS = {"max_steps": 128}
+
+    REWARD_MAX_IG = (
+        1.2
+        + 13 * IG_REWARD_GOAL_CELL_FACTOR * IG_REWARD_BINARY_CELL
+        + IG_REWARD_GOAL_COMPLETION
+        if IG_REWARD_MODE == "binary"
+        else 4.0
+    )  # 6.7 4.0 # 0.2 ## binary 1.2 entropy 4.0 (w/o accumulating)
 
     # IG_CURRICULUM_LEARNING = True
     # IG_CURRICULUM_LEARNING_STEPS_2_OBS = 2000000
@@ -169,7 +177,7 @@ class Config:
         "pos_global_frame",
         "vel_global_frame",
         "local_grid",
-        "ego_binary_map",
+        "mc_ego_binary_goal",
     ]  # occupancy grid
 
     # STATES_IN_OBS = ['radius', 'heading_global_frame', 'angvel_global_frame', 'pos_global_frame', 'vel_global_frame', 'local_grid', 'binary_map']  # occupancy grid
@@ -368,6 +376,14 @@ class Config:
             "size": EGO_MAP_SIZE,
             "bounds": [0, 255],
             "attr": "ig_model.targetMap.bin_ego_map",
+            "std": np.ones((MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.uint8),
+            "mean": np.ones((MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.uint8),
+        },
+        "mc_ego_binary_goal": {
+            "dtype": np.uint8,
+            "size": (2, EGO_MAP_SIZE[0], EGO_MAP_SIZE[1]),
+            "bounds": [0, 255],
+            "attr": "ig_model.targetMap.mc_ego_binary_goal",
             "std": np.ones((MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.uint8),
             "mean": np.ones((MAP_WIDTH_PXL, MAP_HEIGHT_PXL), dtype=np.uint8),
         },

gym_collision_avoidance/envs/information_models/targetMap.py
@@ -99,6 +99,9 @@ class targetMap:
             self.ego_map_inner_size,
         )
 
+        # Multi-Channel Map
+        self.mc_ego_binary_goal = np.stack((self.bin_ego_map, self.goal_ego_map))
+
     def _init_free_cells(self):
 
         self.free_cells.clear()
@@ -309,6 +312,9 @@ class targetMap:
             self.ego_map_inner_size,
         )
 
+        # Multi-Channel Map
+        self.mc_ego_binary_goal = np.stack((self.bin_ego_map, self.goal_ego_map))
+
         # Check Termination
         if Config.IG_THRES_ACTIVE:
             if self.entropy_free_space <= self.thres_entropy:
@@ -327,7 +333,7 @@ class targetMap:
                 self.finished = self.finished_entropy
 
         # Check for completed goals
-        self.check_goal_completion()
+        reward += self.check_goal_completion()
 
         return obsvdCells, reward
 
@@ -391,6 +397,8 @@ class targetMap:
 
     def check_goal_completion(self):
 
+        reward = 0
+
         # Check if goal completed
         completed_goal_idc = []
         for goal_idx in range(len(self.goal_cells)):
@@ -400,11 +408,12 @@ class targetMap:
         completed_goals = []
         completed_goal_idc.sort()
         for goal_idx in reversed(completed_goal_idc):
+            reward += Config.IG_REWARD_GOAL_COMPLETION
             completed_goals.append(self.current_goals[goal_idx])
             del self.current_goals[goal_idx]
             del self.goal_cells[goal_idx]
 
-        return completed_goals
+        return reward
 
     def create_ego_map(self, pose, map, newImageWidth, border_value=0.0):
 

gym_collision_avoidance/experiments/src/test_env.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 def main():
     # Instantiate the environment
     # env = gym.make("CollisionAvoidance-v0")
-    n_envs = 1
+    n_envs = 16
     env, _ = create_env(n_envs=n_envs, subproc=(n_envs > 1))
     # Path to Map
     # mapPath = os.path.abspath(os.path.dirname(__file__)) + "/simple_rooms_no_walls.png"
@@ -46,8 +46,10 @@ def main():
 
     # Repeatedly send actions to the environment based on agents' observations
     n_eps = 1
-    num_steps = 128
+    num_steps = 512
     max_rewards = []
+    max_ig_rewards = []
+    ig_rewards = [[] for i in range(n_envs)]
     rewards = [[] for i in range(n_envs)]
     eps_reward = []
     coverage_finished = 0
@@ -71,6 +73,7 @@ def main():
         for i in range(n_envs):
 
             rewards[i].append(np.squeeze(reward[i]))
+            ig_rewards[i].append(np.squeeze(info[i]['ig_reward']))
 
             if game_over[i].any():
                 # if info[i]["in_collision"]:
@@ -82,6 +85,7 @@ def main():
                 n_eps_steps.append(info[i]["step_num"])
                 n_free_cells_eps.append(info[i]["n_free_cells"])
                 max_rewards.append(np.max(rewards[i]))
+                max_ig_rewards.append(np.max(ig_rewards[i]))
                 eps_reward.append(np.sum(rewards[i]))
                 status.append(
                     0 if info[i]["ran_out_of_time"] or info[i]["in_collision"] else 1
@@ -89,6 +93,7 @@ def main():
                 eps_ids[i] += 1
                 env_ids.append([i, eps_ids[i]])
                 rewards[i] = []
+                ig_rewards[i] = []
             #     rewards = []
             #     print("Avg Episode Reward: " + str(eps_reward))
     env.reset()
@@ -96,6 +101,7 @@ def main():
     # eps_reward = np.sum(np.asarray(rewards)) / n_envs / n_eps
     print("Avg Episode Reward: " + str(np.mean(eps_reward)))
     print("Max Step Rewards: " + str(np.max(max_rewards)))
+    print("Max IG Step Rewards: " + str(np.max(max_ig_rewards)))
     print("N episodes: " + str(len(eps_reward)))
     print("N finished: " + str(coverage_finished))
     print("Avg Steps per Eps: " + str(np.mean(n_eps_steps)))